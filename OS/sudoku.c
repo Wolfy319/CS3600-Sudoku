254f2cc926d9f92f5b3beeec2efe5c41ada6d4de
Wolfy319 <jonwolffiorini@gmail.com>
2021-09-19 19:21:25 -0600

    Got all the checking to work

--- OS/sudoku.c
+++ OS/sudoku.c
@@ -33,7 +33,7 @@ typedef struct indexPro{
    int bottomRow;
    int leftColumn;
    int rightColumn;  
-};
+}Indexer;
 
 /** Reads the sudokuPuzzle.txt into sudokuPuzzle[][] **/
 void sudokuMatrix(){
@@ -47,7 +47,7 @@ void sudokuMatrix(){
         }
 
     }
-    /**printSudokuPuzzle();**/
+    printSudokuPuzzle();
 }
 
 /** Prints the sudoku puzzle from the array in sudoku format **/
@@ -75,7 +75,7 @@ int compareVals(const void * a, const void * b ){
 /** Checks if the given column has all values
  * first sorts array columnValues using std lib qsort
  * */
-void colCheck(struct indexPro col_){
+void colCheck(Indexer col_, int i){
     int colValues[9];
     bool contains = TRUE; 
     
@@ -84,40 +84,79 @@ void colCheck(struct indexPro col_){
     }
 
     qsort(colValues, 9, sizeof(int), compareVals);
-   
-    for(int val = 1; val < 10; val++){
-        for(int mover = 0; mover < 9; mover++){
-            if(colValues[mover] != val){
-                contains = FALSE;
-                break; 
-            }
+    
+    int val = 1;
+    for(int mover = 0; mover < 9; mover++){
+        if(colValues[mover] != val){
+            contains = FALSE;
+            break;
         }
+        val++;
     }
     boolCol[col_.leftColumn] = contains;
     printf("bool check: ");
-    printf("%d ", boolCol[0]);
+    printf("%d ", boolCol[i]);
      
     
     
 }
 
 
-void rowCheck(struct indexPro row_){
+void rowCheck(Indexer row_, int i){
     int rowValues[9];
     bool contains = TRUE;
 
     for(int col = 0; col < 9; col++){
         rowValues[col] = sudokuPuzle[row_.topRow][col];
-        printf("%d ", rowValues[col]);
     }
 
-    for(int u = 0; u < 9; u++){
-        printf("%d ", rowValues[u]);
-    }
+    
 
     qsort(rowValues, 9, sizeof(int), compareVals);
     
+    int val = 1;
+    for(int mover = 0; mover < 9; mover++){
+        if(rowValues[mover] != val){
+            contains = FALSE;
+            break;
+        }
+        val++;
+    }
+    boolCol[row_.leftColumn] = contains;
+    printf("bool check: ");
+    printf("%d ", boolCol[i]);
     
+    
+}
+
+/** Checks if the given column has all values
+ * first sorts array columnValues using std lib qsort
+ * */
+void gridCheck(Indexer grid_, int i){
+    int gridValues[9];
+    bool contains = TRUE; 
+    
+    int index = 0;
+    for(int row = grid_.topRow; row <= grid_.bottomRow; row++){
+        for(int col = grid_.leftColumn; col <= grid_.rightColumn; col++) {
+            gridValues[index] = sudokuPuzle[row][col]; 
+            index++;
+        }
+    }
+
+    qsort(gridValues, 9, sizeof(int), compareVals);
+    
+    int val = 1;
+    for(int mover = 0; mover < 9; mover++){
+        if(gridValues[mover] != val){
+            contains = FALSE;
+            break;
+        }
+        val++;
+    }
+    boolSubGrid[i] = contains;
+    printf("bool check: ");
+    printf("%d ", boolSubGrid[i]);  
 }
 
 
@@ -126,219 +165,47 @@ void rowCheck(struct indexPro row_){
 int main(){
 
 /** Column struct intialization **/
-    struct indexPro col_1; 
-    struct indexPro col_2;
-    struct indexPro col_3;
-    struct indexPro col_4;
-    struct indexPro col_5;
-    struct indexPro col_6;
-    struct indexPro col_7;
-    struct indexPro col_8;
-    struct indexPro col_9;
+    Indexer columns[9];
 
 /** Row struct initialization **/
-    struct indexPro row_1;
-    struct indexPro row_2;
-    struct indexPro row_3;
-    struct indexPro row_4;
-    struct indexPro row_5;
-    struct indexPro row_6;
-    struct indexPro row_7;
-    struct indexPro row_8;
-    struct indexPro row_9;
+    Indexer rows[9];
 
 /** Sub_Grid struct intialization **/
-    struct indexPro grid_1;
-    struct indexPro grid_2;
-    struct indexPro grid_3;
-    struct indexPro grid_4;
-    struct indexPro grid_5;
-    struct indexPro grid_6;
-    struct indexPro grid_7;
-    struct indexPro grid_8;
-    struct indexPro grid_9;
+    Indexer grids[9];
     
 /************************************************************************************************************************************/
 /** Set Column indexs **/
-/**(0,8,0,0)**/
-    col_1.topRow = 0;
-    col_1.bottomRow = 8;
-    col_1.leftColumn = 0;
-    col_1.rightColumn = 0;
+    for(int col = 0; col < 9; col++) {
+        columns[col].topRow = 0;
+        columns[col].bottomRow = 8;
+        columns[col].leftColumn = col;
+        columns[col].rightColumn = col;
+    }
 
-/**(0,8,1,1)**/
-    col_2.topRow = 0;
-    col_2.bottomRow = 8;
-    col_2.leftColumn = 1;
-    col_2.rightColumn = 1;
+    for(int row = 0; row < 9; row++) {
+        rows[row].topRow = row;
+        rows[row].bottomRow = row;
+        rows[row].leftColumn = 0;
+        rows[row].rightColumn = 8;
+    }
 
-/**(0,8,2,2)**/
-    col_3.topRow = 0;
-    col_3.bottomRow = 8;
-    col_3.leftColumn = 2;
-    col_3.rightColumn = 2;
+    int currentGrid = 0;
+    for(int gridX = 0; gridX < 3; gridX++) {
+        for(int gridY = 0; gridY < 3; gridY++) {
+            grids[currentGrid].topRow = gridX * 3;
+            grids[currentGrid].bottomRow = (gridX * 3) + 2;
+            grids[currentGrid].leftColumn = gridY * 3;
+            grids[currentGrid].rightColumn = (gridY * 3) +2;
+            currentGrid++;
+        }
+    }
 
-/**(0,8,3,3)**/
-    col_4.topRow = 0;
-    col_4.bottomRow = 8;
-    col_4.leftColumn = 3;
-    col_4.rightColumn = 3;
+    sudokuMatrix();
+    for(int i = 0; i < 9; i++) {
+        colCheck(columns[i], i);
+        rowCheck(rows[i], i); 
+        gridCheck(grids[i], i);
+    }
 
-/**(0,8,4,4)**/
-    col_5.topRow = 0;
-    col_5.bottomRow = 8;
-    col_5.leftColumn = 4;
-    col_5.rightColumn = 4;
-
-/**(0,8,5,5)**/
-    col_6.topRow = 0;
-    col_6.bottomRow = 8;
-    col_6.leftColumn = 5;
-    col_6.rightColumn = 5;
-
-/**(0,8,6,6)**/    
-    col_7.topRow = 0;
-    col_7.bottomRow = 8;
-    col_7.leftColumn = 6;
-    col_7.rightColumn = 6;
-
-/**(0,8,7,7)**/
-    col_8.topRow = 0;
-    col_8.bottomRow = 8;
-    col_8.leftColumn = 7;
-    col_8.rightColumn = 7;
-
-/**(0,8,8,8)**/
-    col_9.topRow = 0;
-    col_9.bottomRow = 8;
-    col_9.leftColumn = 8;
-    col_9.rightColumn = 8;    
-
-/************************************************************************************************************************************/
-
-/** Set Row indexs **/
-/**(0,0,0,8)**/
-    row_1.topRow = 0;
-    row_1.bottomRow = 0;
-    row_1.leftColumn = 0;
-    row_1.rightColumn = 8;
-
-/**(1,1,0,8)**/
-    row_2.topRow = 1;
-    row_2.bottomRow = 1;
-    row_2.leftColumn = 0;
-    row_2.rightColumn = 8;
-
-/**(2,2,0,8)**/
-    row_3.topRow = 2;
-    row_3.bottomRow = 2;
-    row_3.leftColumn = 0;
-    row_3.rightColumn = 8;
-
-/**(3,3,0,8)**/
-    row_4.topRow = 3;
-    row_4.bottomRow = 3;
-    row_4.leftColumn = 0;
-    row_4.rightColumn = 8;
-
-/**(4,4,0,8)**/
-    row_5.topRow = 4;
-    row_5.bottomRow = 4;
-    row_5.leftColumn = 0;
-    row_5.rightColumn = 8;
-
-/**(5,5,0,8)**/
-    row_6.topRow = 5;
-    row_6.bottomRow = 5;
-    row_6.leftColumn = 0;
-    row_6.rightColumn = 8;
-
-/**(6,6,0,8)**/
-    row_7.topRow = 6;
-    row_7.bottomRow = 6;
-    row_7.leftColumn = 0;
-    row_7.rightColumn = 8;
-
-/**(7,7,0,8)**/
-    row_8.topRow = 7;
-    row_8.bottomRow = 7;
-    row_8.leftColumn = 0;
-    row_8.rightColumn = 8;
-
-/**(8,8,0,8)**/
-    row_9.topRow = 8;
-    row_9.bottomRow = 8;
-    row_9.leftColumn = 0;
-    row_9.rightColumn = 8;    
-
-/************************************************************************************************************************************/
-
-/** Set Sub_Grid indexs **/
-/**(0,2,0,2)**/
-    grid_1.topRow = 0;
-    grid_1.bottomRow = 2;
-    grid_1.leftColumn = 0;
-    grid_1.rightColumn = 2;
-
-/**(0,2,3,5)**/
-    grid_2.topRow = 0;
-    grid_2.bottomRow = 2;
-    grid_2.leftColumn = 3;
-    grid_2.rightColumn = 5;
-
-/**(0,2,6,8)**/
-    grid_3.topRow = 0;
-    grid_3.bottomRow = 2;
-    grid_3.leftColumn = 6;
-    grid_3.rightColumn = 8;
-
-/**(3,5,0,2)**/
-    grid_4.topRow = 3;
-    grid_4.bottomRow = 5;
-    grid_4.leftColumn = 0;
-    grid_4.rightColumn = 2;
-
-/**(3,5,3,5)**/
-    grid_5.topRow = 3;
-    grid_5.bottomRow = 5;
-    grid_5.leftColumn = 3;
-    grid_5.rightColumn = 5;
-
-/**(3,5,6,8)**/
-    grid_6.topRow = 3;
-    grid_6.bottomRow = 5;
-    grid_6.leftColumn = 6;
-    grid_6.rightColumn = 8;
-
-/**(6,8,0,2)**/
-    grid_7.topRow = 6;
-    grid_7.bottomRow = 8;
-    grid_7.leftColumn = 0;
-    grid_7.rightColumn = 2;
-
-/**(6,8,3,5)**/
-    grid_8.topRow = 6;
-    grid_8.bottomRow = 8;
-    grid_8.leftColumn = 3;
-    grid_8.rightColumn = 5;
-
-/**(6,8,6,8)**/
-    grid_9.topRow = 6;
-    grid_9.bottomRow = 8;
-    grid_9.leftColumn = 6;
-    grid_9.rightColumn = 8;
-    
-/************************************************************************************************************************************/
-
-sudokuMatrix();
-colCheck(col_1);
-rowCheck(row_1);
- 
-
-
-
-
-
-
-return 0; 
+    return 0; 
 }
